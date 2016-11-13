a507d33c5681cb16171234a1e0cf15fa181482c4
sf17k <sf171k@gmail.com>
2016-11-12 19:02:23 -0500

    Use Kotlin reflection

--- client/src/com/drakelinglabs/digitaljumper/client/Utils.kt
+++ client/src/com/drakelinglabs/digitaljumper/client/Utils.kt
@@ -2,29 +2,24 @@ package com.drakelinglabs.digitaljumper.client
 
 import com.artemis.Component
 import com.badlogic.gdx.math.Vector2
+import kotlin.reflect.*
 
 object Utils {
 
-    // assumes all Components contain only simple data and Vector2's
-    // warning: some hairy reflection
-    // don't be surprised if it breaks in complicated ways
+    // Assumes all Components contain only simple data and Vector2's.
+    // FIXME: Lags game for a second first time it's called.
     fun copyComponent(src: Component): Component {
-        val cls = src.javaClass
-        val dst = cls.getConstructor().newInstance()
-        cls.declaredFields.forEach {
-            // fields are actually private
-            // guessing because Kotlin hides them behind getters/setters
-            // so we have to use those
-            val cname = it.name.capitalize()
-            val ftype = it.type
-            val getter = cls.getMethod("get" + cname)
-            val setter = cls.getMethod("set" + cname, ftype)
-            if (ftype == Vector2::class.java) {
-                // deep copy Vector2's
-                (getter.invoke(dst) as Vector2).set(getter.invoke(src) as Vector2)
-            } else {
-                // shallow copy everything else
-                setter.invoke(dst, getter.invoke(src))
+        val cls = src.javaClass.kotlin // is this right?
+        val dst = cls.primaryConstructor!!.call()
+        cls.memberProperties.forEach {
+            if (it is KMutableProperty<*>) {
+                if (it.returnType.equals(Vector2::class.defaultType)) {
+                    // deep copy Vector2's
+                    (it.get(dst) as Vector2).set(it.get(src) as Vector2)
+                } else {
+                    // shallow copy everything else
+                    it.setter.call(dst, it.get(src))
+                }
             }
         }
         return dst
